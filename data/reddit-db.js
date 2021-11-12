19af17850b1b97420b5c905ec2e45152ab65d200
Fred Cure <fred_cure@outlook.com>
2021-11-11 21:16:06 -0500

    Users can view a list of posts

--- data/reddit-db.js
+++ data/reddit-db.js
@@ -9,7 +9,8 @@ const url = 'mongodb://127.0.0.1:27017/freddit';
 mongoose.connect(
     url,
     {
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     },
     function (err, db) {
         assert.equal(null, err);
